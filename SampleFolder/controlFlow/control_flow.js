5a0e011a283fbc90bbdecc32772b7773ee3c7b3f
Siljelidj <167670253+Siljelidj@users.noreply.github.com>
2024-04-24 15:50:46 +0200

    uppdaterad

--- SampleFolder/controlFlow/control_flow.js
+++ SampleFolder/controlFlow/control_flow.js
@@ -9,3 +9,33 @@ if (userRole === "admin") {
     acessLevel = ("No acess granted");
 }
 console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+let userType = "subscriber";
+let userCategory;
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
