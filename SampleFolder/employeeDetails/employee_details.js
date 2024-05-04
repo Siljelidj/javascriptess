03c7b626a31230f4bd2208c94d8fce8c483b55db
Siljelidj <167670253+Siljelidj@users.noreply.github.com>
2024-05-04 19:56:21 +0200

    fixed

--- SampleFolder/employeeDetails/employee_details.js
+++ SampleFolder/employeeDetails/employee_details.js
@@ -22,8 +22,9 @@ function findEmployeeById(employeeId) {
     if (foundEmployee) {
     document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     }
-    else{
+    else
+    {
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
 
     }
-    
+  }
